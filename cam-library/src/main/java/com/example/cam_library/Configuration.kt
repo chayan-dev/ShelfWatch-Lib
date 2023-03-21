097690a964625c2636ac76595e942377bfcd1bc5
Chayan Das <chayandas0308@gmail.com>
2023-03-21 17:35:26 +0530

    intent flag to start activity

--- cam-library/src/main/java/com/example/cam_library/Configuration.kt
+++ cam-library/src/main/java/com/example/cam_library/Configuration.kt
@@ -8,6 +8,7 @@ public class Configuration {
   public fun openCameraSDk(context: Context) {
     val intent = Intent(context, CameraActivity::class.java)
     if (intent.resolveActivity(context.packageManager) != null) {
+      intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
       context.startActivity(intent)
     }
   }
